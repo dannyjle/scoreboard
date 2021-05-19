441223ecf1ecf50d284a70f4750bef4ef28b0b47
Danny Le <le.dannyj@gmail.com>
2021-05-19 16:53:30 -0400

    Subtracting Team Score

--- public/main.js
+++ public/main.js
@@ -3,19 +3,57 @@ function main() {
     const teamOneName = document.querySelector('.team1 h2')
     teamOneName.textContent = event.target.value
   }
+  document
+    .querySelector('.team1 input')
+    .addEventListener('input', updateTeamOneName)
 
   function updateTeamTwoName(event) {
     const teamTwoName = document.querySelector('.team2 h2')
     teamTwoName.textContent = event.target.value
   }
-
-  document
-    .querySelector('.team1 input')
-    .addEventListener('input', updateTeamOneName)
-
   document
     .querySelector('.team2 input')
     .addEventListener('input', updateTeamTwoName)
+
+  let teamOneCounter = 0
+  function addTeamOneScore() {
+    teamOneCounter++
+    const teamOneScore = document.querySelector('.team1 h3')
+    teamOneScore.textContent = teamOneCounter
+  }
+  document
+    .querySelector('.team1 .add')
+    .addEventListener('click', addTeamOneScore)
+
+  let teamTwoCounter = 0
+  function addTeamTwoScore() {
+    teamTwoCounter++
+    const teamTwoScore = document.querySelector('.team2 h3')
+    teamTwoScore.textContent = teamTwoCounter
+  }
+  document
+    .querySelector('.team2 .add')
+    .addEventListener('click', addTeamTwoScore)
+
+  // Subtracts Team One's score
+  function subTeamOneScore() {
+    teamOneCounter--
+    const teamOneScore = document.querySelector('.team1 h3')
+    teamOneScore.textContent = teamOneCounter
+  }
+  document
+    .querySelector('.team1 .subtract')
+    .addEventListener('click', subTeamOneScore)
+
+  // Subtracts Team Two's score
+  function subTeamTwoScore() {
+    teamTwoCounter--
+    const teamTwoScore = document.querySelector('.team2 h3')
+    teamTwoScore.textContent = teamTwoCounter
+  }
+  document
+    .querySelector('.team2 .subtract')
+    .addEventListener('click', subTeamTwoScore)
 }
 
 document.addEventListener('DOMContentLoaded', main)
