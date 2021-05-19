0829a89a3c69e347c976ba5ff01f9eeb80e205f2
Danny Le <le.dannyj@gmail.com>
2021-05-19 17:49:23 -0400

    Added :hover and cursor features

--- public/main.js
+++ public/main.js
@@ -15,8 +15,14 @@ function main() {
     .querySelector('.team2 input')
     .addEventListener('input', updateTeamTwoName)
 
+  // Adds to Team One's score
   let teamOneCounter = 0
   function addTeamOneScore() {
+    if (teamOneCounter >= 21) {
+      console.log('WINNER')
+      window.alert('--- CONGRATULATIONS YOUR TEAM WINS!!!! ---')
+      return
+    }
     teamOneCounter++
     const teamOneScore = document.querySelector('.team1 h3')
     teamOneScore.textContent = teamOneCounter
@@ -25,18 +31,13 @@ function main() {
     .querySelector('.team1 .add')
     .addEventListener('click', addTeamOneScore)
 
-  let teamTwoCounter = 0
-  function addTeamTwoScore() {
-    teamTwoCounter++
-    const teamTwoScore = document.querySelector('.team2 h3')
-    teamTwoScore.textContent = teamTwoCounter
-  }
-  document
-    .querySelector('.team2 .add')
-    .addEventListener('click', addTeamTwoScore)
-
   // Subtracts Team One's score
   function subTeamOneScore() {
+    if (teamOneCounter <= 0) {
+      console.log('NO NEGATIVES')
+      window.alert('--- NEGATIVE POINTS ARE NOT ALLOWED ---')
+      return
+    }
     teamOneCounter--
     const teamOneScore = document.querySelector('.team1 h3')
     teamOneScore.textContent = teamOneCounter
@@ -45,8 +46,30 @@ function main() {
     .querySelector('.team1 .subtract')
     .addEventListener('click', subTeamOneScore)
 
+  // Adds to Team Two's score
+  let teamTwoCounter = 0
+  function addTeamTwoScore() {
+    if (teamTwoCounter >= 21) {
+      console.log('WINNER')
+      window.alert('--- CONGRATULATIONS YOUR TEAM WINS!!!! ---')
+      return
+    }
+    teamTwoCounter++
+    const teamTwoScore = document.querySelector('.team2 h3')
+    teamTwoScore.textContent = teamTwoCounter
+  }
+  document
+    .querySelector('.team2 .add')
+    .addEventListener('click', addTeamTwoScore)
+
   // Subtracts Team Two's score
   function subTeamTwoScore() {
+    if (teamTwoCounter <= 0) {
+      console.log('NO NEGATIVES')
+      window.alert('--- NEGATIVE POINTS ARE NOT ALLOWED ---')
+      return
+    }
+
     teamTwoCounter--
     const teamTwoScore = document.querySelector('.team2 h3')
     teamTwoScore.textContent = teamTwoCounter
@@ -54,6 +77,16 @@ function main() {
   document
     .querySelector('.team2 .subtract')
     .addEventListener('click', subTeamTwoScore)
+
+  const resetScore = 0
+  function resetScores(event) {
+    teamOneScore = document.querySelector('.team1 h3')
+    teamTwoScore = document.querySelector('.team2 h3')
+    teamOneScore.textContent = resetScore
+    teamTwoScore.textContent = resetScore
+  }
+
+  document.querySelector('footer').addEventListener('click', resetScores)
 }
 
 document.addEventListener('DOMContentLoaded', main)
